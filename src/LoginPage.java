111f5c481042ecb9e044d6513d5aa1d7d477ee87
abduova <abduovaakbota126@gmail.com>
2026-05-17 23:35:11 +0500

    Create LoginPage.java
    restore login page

---
+++ src/LoginPage.java
@@ -0,0 +1,28 @@
+package console.pages;
+
+import core.UniversityKernel;
+import users.User;
+import console.pagescore.*;
+import parsers.*;
+
+public class LoginPage extends Page {
+    public LoginPage() {
+        super("Login");
+
+        addAction("Login", this::handleLogin);
+    }
+
+    private void handleLogin() {
+        String username = console.getInput().read("Username", new StringParser(false));
+        String password = console.getInput().read("Password", new StringParser(false));
+
+        User user = UniversityKernel.getInstance().findUserByUsername(username);
+
+        if (user != null && user.login(password)) {
+            console.getRenderer().renderSuccess("Welcome, " + user.getFullName());
+            console.login(user);
+        } else {
+            console.getRenderer().renderError("Invalid credentials");
+        }
+    }
+}
